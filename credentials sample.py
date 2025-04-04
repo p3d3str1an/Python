7f7c9ce8ed356dacacb39c172257a0da3d3a4018
p3d3str1an <p3d3str1an@gmail.com>
2025-04-04 14:20:53 +0200

    initial commit

---
+++ credentials sample.py
@@ -0,0 +1,57 @@
+# Database credentials
+DATABASES = {
+    'PROD': {
+        'server': '<your server ip>',
+        'port': '<your server port>',
+        'database': '<your SBO database name>',
+        'username': '<sql username>',
+        'password': '<sql password>'
+    },
+    'TESZT': {
+        'server': '<your server ip>',
+        'port': '<your server port>',
+        'database': '<your test SBO database name>',
+        'username': '<sql username>',
+        'password': '<sql password>'
+    },
+    'ASSIST': {
+        'server': '<your server ip>',
+        'port': '<your server port>',
+        'database': '<your assist database name>',
+        'username': '<sql username>',
+        'password': '<sql password>'
+    },
+    'ARSUNAHU': {
+        'username': '<your cscart instance mysql username>',
+        'password': '<your mysql password>',
+        'host': '<your mysql host>',
+        'port': '<your mysql port>',
+        'database': '<your mysql database name>'
+    }
+}
+
+# Pushover credentials
+PUSHOVER_USER_KEY = '<your pushover user key>'
+PUSHOVER_API_TOKEN = '<your pushover api token>'
+
+# Yagmail credentials
+YAGMAIL_USER = '<your yagmail user>'
+YAGMAIL_PASSWORD = '<your yagmail password>'
+
+# Growatt credentials
+GROWATT_EMAIL = '<your growatt email>'
+GROWATT_PASSWORD = '<your growatt password>'
+
+# Email credentials
+ARSUNA_EMAIL_USER = '<your email user>'
+ARSUNA_EMAIL_PASSWORD = '<your email password>'
+
+# CSCart credentials
+ARSUNAHU_TESZT22_API_KEY = "<your cscart test api key>"
+ARSUNAHU_ELES_API_KEY = "<your cscart eles api key>"
+
+#NAV credentials
+NAV_SIGNATURE_KEY='<your nav technical user signature key>'
+NAV_REPLACEMENT_KEY='<your nav technical user replacement key>'
+NAV_PASSWORD='<your nav technical user password>'
+NAV_USERNAME='<your nav technical user username>'
